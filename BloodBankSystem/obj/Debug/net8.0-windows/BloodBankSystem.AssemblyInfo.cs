fee5a79a88e0a150d3aa28c8f21d0ba003cdde54
minhthong25 <minh35940@gmail.com>
2025-05-28 20:39:48 +0700

    BloodBank

--- BloodBankSystem/obj/Debug/net8.0-windows/BloodBankSystem.AssemblyInfo.cs
+++ BloodBankSystem/obj/Debug/net8.0-windows/BloodBankSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodBankSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bde8747f8304cb320dacc98598cfdfe6baebc1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bfe8cb960dcd50b0709218aec55bc2488ddeb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodBankSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodBankSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
